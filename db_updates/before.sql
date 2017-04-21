a08309e0a31332d05e3bf770f9ec589341ca1522
SNR <xvanok@nag.ru>
2017-04-21 09:48:38 +0500

    update requests_monitor

--- db_updates/before.sql
+++ db_updates/before.sql
@@ -1,4 +1,6 @@
 RENAME TABLE `configs` TO `configs_old`;
 RENAME TABLE `requests` TO `requests_monitor`;
 ALTER TABLE `requests_monitor` ENGINE = MYISAM;
+ALTER TABLE `requests_monitor` CHANGE `fw` `fw` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
+ALTER TABLE `requests_monitor` CHANGE `response` `response` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NULL;
 RENAME TABLE `managers` TO `managers_old`;
